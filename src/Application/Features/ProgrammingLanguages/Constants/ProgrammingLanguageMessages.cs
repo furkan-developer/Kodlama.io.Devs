b9671c8ff95663ad9f2e1d573990acbf35709164
Furkan Aydin <furkan.aydin@intern.veripark.com>
2022-09-05 19:34:06 +0300

    Create CreateProgrammingLanguageHandler

---
+++ src/Application/Features/ProgrammingLanguages/Constants/ProgrammingLanguageMessages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguages.Constants
+{
+    public static class ProgrammingLanguageMessages
+    {
+        public const string ProgrammingLanguageNameIsAlreadyExist = "Programming Language Name is already exist";
+    }
+}
---
+++ src/Application/Features/ProgrammingLanguages/Rules/ProgrammingLanguageRules.cs
@@ -0,0 +1,29 @@
+﻿using Application.Features.ProgrammingLanguages.Constants;
+using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguages.Rules
+{
+    public class ProgrammingLanguageRules
+    {
+        private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+
+        public ProgrammingLanguageRules(IProgrammingLanguageRepository programmingLanguageRepository)
+        {
+            _programmingLanguageRepository = programmingLanguageRepository;
+        }
+
+        public async Task ProgrammingNameCanNotBeDuplicatedWhenInserted(string name)
+        {
+            IPaginate<ProgrammingLanguage> result = await _programmingLanguageRepository.GetListAsync(predicate: p => p.Name == name, enableTracking: false);
+            if (result.Items.Any()) throw new BusinessException(ProgrammingLanguageMessages.ProgrammingLanguageNameIsAlreadyExist);
+        }
+    }
+}
